ddfca8df634df968077f1f1305c80db0ba1af632
devmev10 <ravitokas90@gmail.com>
2023-02-23 11:25:39 +0530

    Add error handling for incorrect id type

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,6 +1,8 @@
 // import workout model
 const Workout = require("../models/workoutModel");
 
+const mongoose = require("mongoose");
+
 // get all workouts
 const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({ createdAt: -1 });
@@ -11,6 +13,12 @@ const getWorkouts = async (req, res) => {
 // get a single workout
 const getWorkout = async (req, res) => {
   const { id } = req.params;
+
+  //   TO check if the id type is correct
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout available" });
+  }
+
   const workout = await Workout.findById(id);
 
   if (!workout) {
