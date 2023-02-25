f1d66a870f681e888f5952ba50db13d4ef6f5dff
devmev10 <ravitokas90@gmail.com>
2023-02-25 15:37:50 +0530

    handle backend api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("api/workouts");
+      const json = response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
       <h2>Home</h2>
