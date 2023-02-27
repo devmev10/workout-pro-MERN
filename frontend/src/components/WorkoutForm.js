354980db045c707807f973ff63abc773076394cb
devmev10 <ravitokas90@gmail.com>
2023-02-27 11:30:48 +0530

    Add error handling for missing field

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,12 +25,15 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
 
     if (response.ok) {
       setTitle("");
       setLoad("");
       setReps("");
+      setError(null);
+      setEmptyFields([]);
       console.log("New Workout added", json);
       dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
@@ -44,6 +48,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in KG):</label>
@@ -51,6 +56,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps:</label>
@@ -58,6 +64,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <button>Add Workout</button>
